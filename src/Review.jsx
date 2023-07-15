7ce7195c461928f8f2a59ad8d24bf4272ce340e6
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-15 10:05:29 +0100

    Update Review.jsx

--- src/Review.jsx
+++ src/Review.jsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
 import people from './data';
-// import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(3)
+  const {name, job, image, text} = people[index]
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className='person-img' />
+        <span className="quote-icon">
+        <FaQuoteRight />
+        </span>
+      </div>
+    </article>
+  )
 };
 
 export default Review;
